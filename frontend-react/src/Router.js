967f44e4b973d9bb1d9d053e8aba3199159e6909
= <113594121+ericsubirana@users.noreply.github.com>
2024-05-04 17:44:32 +0200

    createForm

--- frontend-react/src/Router.js
+++ frontend-react/src/Router.js
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
+import CreateGame from './components/createGame';
+
 function Routers(){
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="*" element={<Home/>} />
+        <Route path="/createGame" element={<CreateGame/>} />
       </Routes>
     </BrowserRouter>
   )
--- frontend-react/src/components/Home.js
+++ frontend-react/src/components/Home.js
@@ -1,17 +1,26 @@
 import {React} from 'react'
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
     const [data, setData] = useState(null);
+    const navigation = useNavigate();
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(data => setData(data));
     }, []);
+
+    const createGame = () => {
+        navigation('/createGame');
+    }
+
     return (
         <div>
             {console.log('Home')}
             <h1>HOME</h1>
+            <button onClick={createGame}>Create Game</button>
         </div>
     )
 }
