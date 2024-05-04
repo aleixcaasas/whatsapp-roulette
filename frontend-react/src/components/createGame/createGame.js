dd3ae2e1342edd5223c5e9df468a9eac788d00e9
aleixcaasas <acasasbarco@gmail.com>
2024-05-04 18:46:13 +0200

    fix problems

--- frontend-react/src/components/createGame/createGame.js
+++ frontend-react/src/components/createGame/createGame.js
@@ -1,41 +1,64 @@
-import {React} from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+import { React } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { FaArrowLeft } from "react-icons/fa";
 
-import './createGame.css';
+import "./createGame.css";
 
-const CreateGame = () => { 
-    const [file, setFile] = useState(null);
-    const [name, setName] = useState('');
+const CreateGame = () => {
+	const [file, setFile] = useState(null);
+	const [name, setName] = useState("");
 
-    const sendForm = () => {
-        axios.post('http://localhost:3000/create-game', { zip: file, username: name })
-         .then(response => {
-             console.log("Success:", response);
-             // Handle success, maybe show a success message or redirect
-         })
-         .catch(error => {
-             console.error("Error:", error);
-             // Handle error, maybe show an error message to the user
-         });
-    }
+	const sendForm = (event) => {
+		event.preventDefault(); // Evitar que el formulario se envíe automáticamente
 
-    return (
-        <div className='divCreate'>
-            <div className='titleCreateGame'>
-                <FaArrowLeft size={25} className='arrowleft' />
-                <h1 className='create-game'>Create Game</h1>
-            </div>
-            <form className='formCreate' onSubmit={sendForm}>
-                <label><h3>Zip File</h3></label>
-                <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-                <label><h3>Username</h3></label>
-                <input className="button-56" type="text" onChange={(e) => setName(e.target.value)} />
-                <button className="button-55" type="submit">Create Game</button>
-            </form>
-        </div>
-    )
-}
+		const formData = new FormData(); // Crear un objeto FormData para enviar datos de formulario y archivos
+
+		formData.append("zip", file); // Agregar el archivo al formulario
+		formData.append("username", name); // Agregar el nombre de usuario al formulario
+
+		axios
+			.post("http://localhost:4000/create-game/", formData, {
+				headers: { "Content-Type": "multipart/form-data" }, // Establecer el encabezado Content-Type adecuado para archivos
+			})
+			.then((response) => {
+				console.log("Success:", response);
+				// Handle success, maybe show a success message or redirect
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+				// Handle error, maybe show an error message to the user
+			});
+	};
+
+	return (
+		<div className="divCreate">
+			<div className="titleCreateGame">
+				<FaArrowLeft size={25} className="arrowleft" />
+				<h1 className="create-game">Create Game</h1>
+			</div>
+			<form className="formCreate" onSubmit={sendForm}>
+				<label>
+					<h3>Zip File</h3>
+				</label>
+				<input
+					type="file"
+					onChange={(e) => setFile(e.target.files[0])}
+				/>
+				<label>
+					<h3>Username</h3>
+				</label>
+				<input
+					className="button-56"
+					type="text"
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<button className="button-55" type="submit">
+					Create Game
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default CreateGame;
