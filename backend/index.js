24e8ebfe8fb9b7d43b7d639c0d1005627693355b
aleixcaasas <acasasbarco@gmail.com>
2024-05-04 17:47:56 +0200

    Start game endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const app = express();
 
 const PORT = 3000;
 const routes = require("./routes/routes.js"); // Se importan las rutas correctamente
 const bodyParser = require("body-parser");
-
-const { connectDB } = require("./database/db.js");
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -15,11 +13,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms")
 );
-
-app.use("/", routes); // Se utiliza el objeto de rutas importado correctamente
-
-//connectDB();
+app.use("/", routes);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
+
+const WebSocket = require("ws");
+/*const wss = new WebSocket.Server({ server });
+
+const activeSockets = [];
+wss.on("connection", (ws) => {
+	console.log("Cliente conectado");
+	activeSockets.push(ws);
+
+	// Manejar mensajes entrantes desde el cliente
+	ws.on("message", (message) => {
+		console.log(`Mensaje recibido: ${message}`);
+
+		// Aquí puedes agregar la lógica para procesar y responder a los mensajes recibidos desde el cliente
+		// Por ejemplo:
+		// ws.send("Mensaje recibido correctamente");
+	});
+
+	// Manejar eventos de cierre de conexión
+	ws.on("close", () => {
+		console.log("Cliente desconectado");
+		activeSockets.splice(activeSockets.indexOf(ws), 1);
+	});
+});*/
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
 	createGame,
 	joinGame,
+    startGame
 } = require("../controllers/manageGame.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/create-game", upload.single("zip"), createGame);
 router.post("/join-game", joinGame);
+router.post("/start-game", startGame)
 
 module.exports = router;
