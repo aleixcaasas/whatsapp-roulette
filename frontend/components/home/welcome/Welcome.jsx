6825b82b1c55ec33a686b67c8b82fb03c1890b19
= <113594121+ericsubirana@users.noreply.github.com>
2024-05-04 12:33:35 +0200

    forIsaacNewtown

--- frontend/components/home/welcome/Welcome.jsx
+++ frontend/components/home/welcome/Welcome.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import {useRouter} from 'expo-router'
 
 import styles from './welcome.style'
+import {COLORS, icons, images, SIZES, FONTS} from '../../../constants'	
 
 const Welcome = () => {
+  const router = useRouter();
+
+  const [pep, setPep] = useState([""]);
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello "NAME"</Text>
+        <Text style={styles.welcomeMessage}>Find your job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+        <TextInput style={styles.searchInput} placeholder='JOIN PARTY' onChangeText={(text) => {setPep(text);}}>
+        </TextInput>
+        </View>
+      </View>
     </View>
   )
 }
