472240bdc47f474f7e9aa2bcc5ec5c62e503b307
= <113594121+ericsubirana@users.noreply.github.com>
2024-05-05 06:24:48 +0200

    mergePotent

--- frontend/src/components/createGame/createGame.js
+++ frontend/src/components/createGame/createGame.js
@@ -16,6 +16,9 @@ const CreateGame = () => {
 	const location = useLocation();
 	const [lobby, setLobbyUsers] = useState([]);
 	const [gameId, setGameId] = useState("");
+	const [question, setQuestion] = useState(null);
+	const [users, setUsers] = useState([]);
+	const [startGameVisible, setStartGameVisible] = useState(true);
 
 	const [veDeFora, setVeDeFora] = useState(true);
 
@@ -30,16 +33,16 @@ const CreateGame = () => {
 			setGameId(gameParam);
 		}
 	}, [location.search]);
-	
 
-	socket.on('epa', (data) => { 
+
+	socket.on('epa', (data) => {
 		axios.post("http://localhost:4000/lobby", { gameId: data.gameId })
-		.then((response) => {
-			setLobbyUsers(response.data);
-		})
-		.catch((error) => {
-			console.error("Error:", error);
-		});
+			.then((response) => {
+				setLobbyUsers(response.data);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
 	});
 
 	const sendForm = (event) => {
@@ -49,8 +52,7 @@ const CreateGame = () => {
 		formData.append("zip", file);
 		formData.append("username", name);
 
-		axios
-			.post("http://localhost:4000/create-game/", formData, {
+		axios.post("http://localhost:4000/create-game/", formData, {
 				headers: { "Content-Type": "multipart/form-data" },
 			})
 			.then((response) => {
@@ -81,6 +83,75 @@ const CreateGame = () => {
 				document.getElementById('uploadImage_id').src = zipIcon
 			}
 		});
+	};
+
+	const startGame = () => {
+		axios.post("http://localhost:4000/start-game", { gameId: gameId })
+			.then((response) => {
+				if (response.data.ok === true) {
+					setUsers(response.data.users);
+					axios.get(`http://localhost:4000/new-round?gameId=${gameId}`)
+						.then((response) => {
+							if (
+								response.headers["content-type"].startsWith(
+									"application/octet-stream"
+								)
+							) {
+								const imageUrl = window.URL.createObjectURL(
+									response.data
+								);
+								const imgElement =
+									document.createElement("img");
+								imgElement.src = imageUrl;
+								const imageContainer =
+									document.getElementById("image-container");
+								imageContainer.appendChild(imgElement);
+							} else if (
+								response.headers["content-type"].startsWith(
+									"audio"
+								)
+							) {
+								const audioUrl = window.URL.createObjectURL(
+									response.data
+								);
+								const audioElement =
+									document.createElement("audio");
+								audioElement.src = audioUrl;
+								audioElement.controls = true;
+								const audioContainer =
+									document.getElementById("audio-container");
+								audioContainer.appendChild(audioElement);
+								audioElement.play();
+							} else {
+								setQuestion(response.data.message);
+								setStartGameVisible(false)
+							}
+						});
+				}
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	};
+
+	const submitVote = (user) => {
+		console.log("Voting for: ", user);
+		axios
+			.post(
+				"http://localhost:4000/vote",
+				{
+					gameId: gameId,
+					player: name,
+					vote: user,
+				},
+				{ headers: { "Content-Type": "application/json" } }
+			)
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
 	};
 
 	const nextRound = () => {
@@ -140,7 +211,7 @@ const CreateGame = () => {
 
 	return (
 		<div id="createGameContainer_id" class="createGameContainer">
-			{!veDeFora ? (
+			{!veDeFora && startGameVisible ? (
 				<div>
 					<div id="createGameHeader_id" class="createGameHeader">
 						<div id="usernameHeader_id" class="usernameHeader">{name}</div>
@@ -168,7 +239,7 @@ const CreateGame = () => {
 			) : null}
 
 			<div>
-				{lobby.length > 0 ? (
+				{lobby.length > 0 && startGameVisible ? (
 
 					<div id="lobbyPlayersContainer_id" class="lobbyPlayersContainer">
 						<div class="playersLobbyText">Players Lobby</div>
@@ -203,12 +274,12 @@ const CreateGame = () => {
 					</div>
 					<div id="gamePanelBottomContainer_id" class="gamePanelBottomContainer">
 						{users ? users.map((user, index) => (
-								<button class="indexUserButton" onClick={()=>submitVote(user)} key={index}>{user}</button>
+							<button class="indexUserButton" onClick={() => submitVote(user)} key={index}>{user}</button>
 						)) : null}
 					</div>
 					<button class="playButtonNextGame" onClick={nextRound}>NEXT GAME</button>
 				</div>
-			) : null}	
+			) : null}
 		</div>
 	)
 };
